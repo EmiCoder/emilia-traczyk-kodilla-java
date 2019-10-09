f37e0d7dea2bdecf1b4414c9967a68e7f8bd1fcb
EmiCello <luktraczyk@vp.pl>
2019-10-09 13:42:19 +0200

    6.2 updated

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,23 @@
 package com.kodilla.testing;
 
+import calculator.Calculator;
+
 public class TestingMain {
 
     public static void main(String[] args) {
 
+        Calculator calculator = new Calculator();
 
+        if (calculator.add(2,5) == 7) {
+            System.out.println("Method add from Calculator class works perfectly");
+        } else {
+            System.out.println("Method add from Calculator class works wrong");
+        }
+
+        if (calculator.subtract(5,1) == 4) {
+            System.out.println("Method subtract from Calculator class works perfectly");
+        } else {
+            System.out.println("Method subtract from Calculator class works wrong");
+        }
     }
 }
