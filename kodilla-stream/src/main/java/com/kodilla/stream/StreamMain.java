dee30ce006b165a63a92436277f95d5a4a171f6e
EmiCello <luktraczyk@vp.pl>
2019-10-16 12:31:04 +0200

    immutable ForumUser added

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,6 +3,7 @@ package com.kodilla.stream;
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.UniqueBeautifier;
 import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExecuteSaySomething;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
@@ -12,11 +13,14 @@ public class StreamMain {
 
     public static void main(String[] args) {
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        System.out.println(poemBeautifier.beautify("mama", (a) -> a + " i tata"));
-        System.out.println(poemBeautifier.beautify("różne wielkości liter", (a) -> a.toUpperCase()));
-        System.out.println(poemBeautifier.beautify("abecadlo", UniqueBeautifier::separate));
-        System.out.println(poemBeautifier.beautify("wakacje", UniqueBeautifier::changeBeginningEnd));
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+
+//        PoemBeautifier poemBeautifier = new PoemBeautifier();
+//        System.out.println(poemBeautifier.beautify("mama", (a) -> a + " i tata"));
+//        System.out.println(poemBeautifier.beautify("różne wielkości liter", (a) -> a.toUpperCase()));
+//        System.out.println(poemBeautifier.beautify("abecadlo", UniqueBeautifier::separate));
+//        System.out.println(poemBeautifier.beautify("wakacje", UniqueBeautifier::changeBeginningEnd));
 
 //        Processor processor = new Processor();
 //        ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
