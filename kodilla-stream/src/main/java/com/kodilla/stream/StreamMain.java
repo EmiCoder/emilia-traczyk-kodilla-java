202c2a60610efb40cb831499f7c153e410f09c67
EmiCello <luktraczyk@vp.pl>
2019-10-16 17:15:18 +0200

    StreamMain updated

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,4 +1,6 @@
 package com.kodilla.stream;
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.beautifier.UniqueBeautifier;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 
@@ -11,14 +13,20 @@ public class StreamMain {
 
     public static void main(String[] args) {
 
-        Forum forum = new Forum();
-        Map<Integer, ForumUser> mapOfForumUsers = forum.getUserList().stream().filter(forumUser -> forumUser.getGender() == 'M')
-                                                    .filter(forumUser -> (Year.now().getValue() - forumUser.getBirthDate().getYear()) >= 20)
-                                                    .filter(forumUser -> forumUser.getAmountOfPublishedPosts() >= 1)
-                                                    .collect(Collectors.toMap(ForumUser::getIdUser, forumUser -> forumUser));
+//        Forum forum = new Forum();
+//        Map<Integer, ForumUser> mapOfForumUsers = forum.getUserList().stream().filter(forumUser -> forumUser.getGender() == 'M')
+//                                                    .filter(forumUser -> (Year.now().getValue() - forumUser.getBirthDate().getYear()) >= 20)
+//                                                    .filter(forumUser -> forumUser.getAmountOfPublishedPosts() >= 1)
+//                                                    .collect(Collectors.toMap(ForumUser::getIdUser, forumUser -> forumUser));
+//
+//        mapOfForumUsers.entrySet().stream()
+//                                  .map(entry -> entry.getKey() + ": " + entry.getValue())
+//                                  .forEach(System.out::println);
 
-        mapOfForumUsers.entrySet().stream()
-                                  .map(entry -> entry.getKey() + ": " + entry.getValue())
-                                  .forEach(System.out::println);
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        System.out.println(poemBeautifier.beautify("mama i tata", String::toUpperCase));
+        System.out.println(poemBeautifier.beautify("wielkość liter", textToBeautify -> textToBeautify.toUpperCase()));
+        System.out.println(poemBeautifier.beautify("kolejneliteryosobno", UniqueBeautifier::separate));
+        System.out.println(poemBeautifier.beautify("wakacje", UniqueBeautifier::changeBeginningEnd));
     }
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/UniqueBeautifier.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/UniqueBeautifier.java
@@ -3,12 +3,10 @@ package com.kodilla.stream.beautifier;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class UniqueBeautifier {
 
     public static String separate (String text) {
-
         List<String> list = new ArrayList<>();
         for (int i = 0; i < text.length(); i++) {
             list.add(String.valueOf(text.charAt(i)));
