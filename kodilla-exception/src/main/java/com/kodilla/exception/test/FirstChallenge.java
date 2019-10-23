f6830a0132a39912630be685c58fd7a4c47c0cd6
EmiCello <luktraczyk@vp.pl>
2019-10-23 21:43:50 +0200

    First Challenge updated

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -4,20 +4,22 @@ public class FirstChallenge {
 
     public static void main(String[] args) {
         FirstChallenge firstChallenge = new FirstChallenge();
-        double result = firstChallenge.divide(3, 0);
-        System.out.println(result);
-    }
-
-    public double divide(double a, double b) {
-        double result = 0;
+        int a = 3;
+        int b = 0;
         try {
-            if (b == 0) throw new ArithmeticException();
-            result = a / b;
+            System.out.println(firstChallenge.divide(a,b));
         } catch (ArithmeticException e) {
-            System.out.println ("You Shouldn't divide a number by zero");
-        } finally {
+            System.out.println("You Shouldn't divide a number by zero");
+        }  finally {
             System.out.println("Division action: " + a + " / " + b);
         }
-        return result;
+
+    }
+
+    public double divide(double a, double b) throws ArithmeticException{
+        if(b == 0){
+            throw new ArithmeticException();
+        }
+        return a / b;
     }
 }
